1e16f7e64cf7b7e8f338c0f13074ba961f4313f4
Md. Mehedi Hasan <m.hasanjoy13@gmail.com>
2023-05-02 19:15:04 +0600

    quiz

--- src/function.ts
+++ src/function.ts
@@ -46,3 +46,16 @@ const myBestFriend: {
 };
 const { fullName } = myBestFriend;
 console.log(fullName);
+
+// Quiz
+function generateAdder(a: number): (b: number) => number {
+  return function (b: number) {
+    return a + b;
+  };
+}
+
+const addTwo = generateAdder(2);
+
+console.log(addTwo(3));
+
+console.log(addTwo(5));
